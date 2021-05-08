e626f0c41c51fc0f82903f3206c5d8718431ae51
Shisun <worlddominator007@gmail.com>
2021-05-08 18:13:56 +0700

    done for mobile

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const date = document.querySelector("#date");
 const toggler = document.querySelector(".toggler");
-const navLink = document.querySelector(".nav__link");
+const navLink = document.querySelector(".nav-link");
 const scroll = document.querySelector(".scroll");
 const about = document.querySelector("#about");
-const brand = document.querySelector('.brand')
-const navBrand = document.querySelector(".navbrand");
+const brand = document.querySelector(".brand");
+const navBrand = document.querySelector(".navBrand");
+const videoContainer = document.querySelectorAll(".video-container");
 
 const setDate = () => {
   const now = new Date();
   date.innerText = now.getFullYear();
 };
-
-window.onload = setDate;
-
-toggler.addEventListener("click", () => {
-  toggler.classList.toggle("active");
-  navLink.classList.toggle("collapsed");
-});
 
 const scrollProgress = () => {
   const max = document.body.scrollHeight - window.innerHeight;
@@ -26,14 +20,45 @@ const scrollProgress = () => {
 const changeNavColor = () => {
   if (window.pageYOffset > about.offsetTop) {
     navBrand.classList.add("solid");
-    brand.classList.remove('collapsed')
+    brand.classList.remove("collapsed");
   } else {
     navBrand.classList.remove("solid");
-    brand.classList.add('collapsed')
+    brand.classList.add("collapsed");
   }
 };
+
+const playVideoOnHover = function (e) {
+  const video = this.children[0];
+  video.play();
+};
+
+const pauseVideoOnHover = function (e) {
+  const video = this.children[0];
+  video.pause();
+};
+
+const playVideoOnClick = function (e) {
+  const video = this.children[0];
+  if (video.paused) {
+    return video.play();
+  }
+  video.pause();
+};
+
+window.onload = setDate;
+
+toggler.addEventListener("click", () => {
+  toggler.classList.toggle("active");
+  navLink.classList.toggle("collapsed");
+});
 
 window.addEventListener("scroll", () => {
   scrollProgress();
   changeNavColor();
+});
+
+videoContainer.forEach((e) => {
+  e.addEventListener("mouseenter", playVideoOnHover);
+  e.addEventListener("mouseleave", pauseVideoOnHover);
+  e.addEventListener("click", playVideoOnClick);
 });
